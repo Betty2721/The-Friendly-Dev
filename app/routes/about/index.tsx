d83d38eb4c2763b4a54a5a09df80249575ff4505
Betty <bethelhemlegesse27@gmail.com>
2025-12-26 10:19:25 +0300

    first commit

---
+++ app/routes/about/index.tsx
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+  return (
+    <>
+      <h2 className="text-3xl font-bold mb-2">
+        Hey, I'm Betty 👋
+      </h2>
+    </>
+  );
+}
+
+export default AboutPage
---
+++ app/routes/home/index.tsx
@@ -0,0 +1,16 @@
+import type { Route } from "./+types/index";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "The Friendly Dev" },
+    { name: "description", content: "Custom website development" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <>
+      HomePage
+    </>
+  );
+}
